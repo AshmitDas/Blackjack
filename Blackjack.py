1d5271e691bdf443627e8dd8147bc2c122bcdf68
AshmitDas <ashmit454@gmail.com>
2021-08-23 22:00:59 +0530

    Adding the PUSH function and completed the main structure of the blackjack.

--- Blackjack.py
+++ Blackjack.py
@@ -1,5 +1,12 @@
+from Projects.Blackjack.black_subpack.push import push
+from Projects.Blackjack.black_subpack.dealer_busts import dealer_busts
+from Projects.Blackjack.black_subpack.player_wins import player_win
+from Projects.Blackjack.black_subpack.player_busts import player_busts
+from Projects.Blackjack.black_subpack.hit_or_stand import hit_or_stand
+from Projects.Blackjack.black_subpack.show_some import show_some
+from Projects.Blackjack.black_subpack.take_bet import take_bet
 from Projects.Blackjack.black_subpack.take_hit import take_hit
-from Projects.Blackjack.black_subpack import *
+# from Projects.Blackjack.black_subpack import *
 import random
 
 suits = ('Hearts', 'Spades', 'Diamond', 'Clubs')
@@ -73,7 +80,10 @@ class Chips:
         self.bet = 0
 
     def bet_won(self):
-        self.total += (self.bet * 1.5) 
+        self.total += self.bet
+
+    def bet_blackjack(self):
+        self.total += (self.bet * 1.5)
 
     def bet_lost(self):
         self.total-=self.bet
@@ -92,5 +102,77 @@ while True:
         take_hit(players_hand,deck)
         take_hit(dealers_hand,deck)
 
+    # Prompting player to enter total amount 
     total_amount = int(input("Enter the total amount of money to place: "))
     players_chip = Chips(total_amount)
+
+
+    #Taking players bet amount
+    take_bet(players_chip)
+
+    #showing players all cards and dealers second card
+    show_some(players_hand,dealers_hand)
+
+    while True:
+
+        #Asking player if he/she want to stand or hit
+        hit_or_stand(players_hand,deck)
+
+        #show all the cards of players hand but keeping one card hidden in dealers hand
+        show_some(players_hand,dealers_hand)
+
+        #if players hand exceeds 21, calling player_busts function and breaking out of the loop
+        if players_hand.value > 21:
+
+            player_busts(players_hand,dealers_hand,players_chip)
+            break
+
+        #If players hand is equal to 21, Its a blackjack, callng function player_win and breaks out of the loop
+        elif players_hand.value == 21:
+
+            print("Its a Blackjack!")
+            player_win(players_hand,dealers_hand,players_chip)
+            break
+
+        else:
+
+            #if dealers value is less than 17, adding card to dealers hand until its equal to 17 or greater than that
+            while dealers_hand.value < 17:
+
+                take_hit(dealers_hand,deck)
+
+            
+            #If dealers value exceeds 21, player wins and breaks out of the loop
+            if dealers_hand.value > 21:
+
+                dealer_busts(players_hand, dealers_hand, players_chip)
+                break
+
+            #if players value exceeds that of dealer, calling player_win function
+            elif players_hand.value > dealers_hand.value:
+                
+                player_win(players_hand, dealers_hand, players_chip)
+                break
+
+            #if dealers value exceeds players value, player loses
+            elif dealers_hand.value > players_hand.value:
+
+                player_busts(players_hand, dealers_hand, players_chip)
+                break
+
+            #if dealers value is equal to that of players value
+            else:
+
+                push()
+                break
+
+        
+    play_again = input("Do you want to play again? \nType 'y' or 'n': ")
+
+    if play_again[0].lower() == 'y':
+        continue
+
+    else:
+
+        print("Thank you for playing!")
+        break
