9aed5442c902c0137153ddd5f123c69f66827054
Ashmit <56751040+AshmitDas@users.noreply.github.com>
2021-08-30 20:41:12 +0530

    Update hit_or_stand.py

--- black_subpack/hit_or_stand.py
+++ black_subpack/hit_or_stand.py
@@ -3,7 +3,7 @@ from .take_hit import take_hit
 def hit_or_stand(player,deck):
 
     while True:
-        player_response = input("Would you like to hit or stand? Either type 'h' or 's'.\n: ")
+        player_response = input("\nWould you like to hit or stand? Either type 'h' or 's'.\n: ")
 
         if player_response[0].lower() == 'h':
             take_hit(player,deck)
